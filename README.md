2d2dcc28d1b79f3ee208545104c8b502820fe96c
karimkhattaby <18755153+karimkhattaby@users.noreply.github.com>
2020-06-12 20:48:23 +0400

    Add Problem 380

--- README.md
+++ README.md
@@ -11,3 +11,4 @@ Here's a table with all questions available in this repo for easy access. The ta
 | :---: | :-----------: | :---: | :---: | :---: |
 | 392 | Is Subsequence | easy | [Problem](https://leetcode.com/problems/is-subsequence) | [Solution](https://github.com/karimkhattaby/Leetcode/blob/master/easy/2%20pointers/Is%20Subsequence.py)
 | 35 | Search Insert Position | easy | [Problem](https://leetcode.com/problems/search-insert-position/) | [Solution](https://github.com/karimkhattaby/Leetcode/blob/master/easy/Binary%20Search/Search%20Insert%20Position.py)
+| 380 | Insert Delete GetRandom O(1) | medium | [Problem](https://leetcode.com/problems/insert-delete-getrandom-o1/) | [Solution](https://github.com/karimkhattaby/Leetcode/blob/master/medium/OOD/Insert%20Delete%20GetRandom%20O%281%29.py)
---
+++ medium/OOD/Insert Delete GetRandom O(1).py
@@ -0,0 +1,39 @@
+class RandomizedSet:
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.data_map = {}
+        self.data_list = []
+
+    def insert(self, val: int) -> bool:
+        """
+        Inserts a value to the set. Returns true if the set did not already contain the specified element.
+        """
+        if val in self.data_map:
+          return False
+        self.data_map[val] = len(self.data_list)
+        self.data_list.append(val)
+        return True
+
+    def remove(self, val: int) -> bool:
+        """
+        Removes a value from the set. Returns true if the set contained the specified element.
+        """
+        if val not in self.data_map:
+          return False
+        
+        idx = self.data_map[val]
+        self.data_list[idx] = self.data_list[-1]
+        self.data_map[self.data_list[idx]] = idx
+        
+        del self.data_map[val]
+        del self.data_list[-1]
+        
+        return True
+
+    def getRandom(self) -> int:
+        """
+        Get a random element from the set.
+        """
+        return random.choice(self.data_list)
